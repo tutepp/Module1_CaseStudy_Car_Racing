941478e3b15d07712883bb2ec17a7791ddead039
tutepp <79822854+tutepp@users.noreply.github.com>
2021-03-31 11:17:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,17 @@ ctx = canvas.getContext('2d');
 
 const redCarImg = new Image();
 redCarImg.src = "img/red_car.png";
-<<<<<<< HEAD
-// cho input img canvas
-=======
 // cho input ảnh bằng canvas
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
-const whiteCarImg = new Image();
-whiteCarImg.src = "img/white_car.png";
 const blueCarImg = new Image();
 blueCarImg.src = "img/blue_car.png";
+const yellowCarImg = new Image();
+yellowCarImg.src = "img/yellow-car.png";
 /// background race
 const backgroundImg = new Image();
 backgroundImg.src = "img/race.jpg";
 // background endgame
 const endGameImg = new Image();
 endGameImg.src = "img/accident.jpg";
-<<<<<<< HEAD
-
-class MainCar {
-    constructor(image, x, y, width, height) {
-        this.image = image;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.speed = 100;
-        this.down = 3;
-
-    }
-
-    // x=x+speed turn right
-=======
 
 
 class MainCar {
@@ -49,7 +29,6 @@ class MainCar {
     }
 
     //tọa độ x=x+speed trả về di chuyển bên phải
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
     moveRight() {
         this.x += this.speed;
         if (this.x + this.width > canvas.width) {
@@ -58,11 +37,7 @@ class MainCar {
         }
     }
 
-<<<<<<< HEAD
-    // x=x-speed turn left
-=======
     //tọa độ x=x-speed trả về di chuyển bên trái
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
     moveLeft() {
         this.x -= this.speed;
         if (this.x < 0) {
@@ -70,19 +45,11 @@ class MainCar {
         }
     }
 
-<<<<<<< HEAD
-    //opposite car
-    carDown() {
-        this.y += this.down;
-    }
-//random position white car
-=======
     //ô tô đối diện xuống
     carDown() {
         this.y += this.down;
     }
 
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
     changeCarPosition1() {
         if (this.y > canvas.height + (this.height * 1)) {
             this.y = 0 - this.height;
@@ -91,11 +58,7 @@ class MainCar {
             this.y += this.down;
         }
     }
-<<<<<<< HEAD
-//random position white car
-=======
 
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
     changeCarPosition2() {
         if (this.y > canvas.height + (this.height * 2)) {
             this.y = 0 - this.height * 5;
@@ -104,19 +67,16 @@ class MainCar {
             this.y += this.down;
         }
     }
-<<<<<<< HEAD
-=======
 
     //hàm vẽ ra ô tô
     draw() {
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 }
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
 
 const redCar = new MainCar(redCarImg, 100, 120, 100, 30);
-const whiteCar = new MainCar(whiteCarImg, 200, 0, 100, 30);
-const blueCar = new MainCar(blueCarImg, 0, 0, 100, 30);
+const blueCar = new MainCar(blueCarImg, 200, 0, 100, 30);
+const yellowCar = new MainCar(yellowCarImg, 0, 0, 100, 30);
 // dùng event keycode để gán phím di chuyển cho nhân vật ô tô đỏ (sang phải:39 ; sang trái:37)
 document.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 39) {
@@ -130,29 +90,6 @@ document.addEventListener('keydown', function (evt) {
 let pause = true;
 let count = 0;
 
-<<<<<<< HEAD
-    // draw car
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
-}
-const redCar = new MainCar(redCarImg, 100, 120, 100, 30);
-const whiteCar = new MainCar(whiteCarImg, 200, 0, 100, 30);
-const blueCar = new MainCar(blueCarImg, 0, 0, 100, 30);
-// eventkeycode (right:39 ; left:37)
-document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 39) {
-        redCar.moveRight();
-    } else if (evt.keyCode === 37) {
-        redCar.moveLeft();
-    }
-})
-//=========================================================================
-let pause = true;
-let count = 0;
-// car accident
-=======
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
 function checkLose(player, character) {
     let x1, y1, w1, h1, x2, y2, w2, h2;
     x1 = player.x;
@@ -167,55 +104,33 @@ function checkLose(player, character) {
         ((x1 + w1) > x2 && (x1 + w1) < (x2 + w2 + w1)) && ((y1 + h1) > y2 && (y1 + h1) < (y2 + h2 + h1))
     ) {
         pause = false;
-<<<<<<< HEAD
-        mainMusic.pause(); // pause maint music
-        gameOverMusic.play();// play endmusic
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // delete canvas
-        ctx.drawImage(endGameImg, 0, 0, canvas.width, canvas.height); // draw image accident
-=======
+        mainMusic.pause();
+        gameOverMusic.play()
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(endGameImg, 0, 0, canvas.width, canvas.height);
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
 
 
     }
 }
-<<<<<<< HEAD
-// function start
-function start() {
-    if (pause) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height); // draw background
-        mainMusic.play(); // play maint music
-=======
 
 function start() {
     if (pause) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
+        mainMusic.play();
         redCar.draw();
+        yellowCar.draw();
+        yellowCar.changeCarPosition1();
+        blueCar.changeCarPosition2();
         blueCar.draw();
-        blueCar.changeCarPosition1();
-        whiteCar.changeCarPosition2();
-        whiteCar.draw();
         count++;
         ctx.fillStyle = "white";
         ctx.font = "10px"
-<<<<<<< HEAD
-        ctx.fillText("Score:" + " " + Math.floor(count / 100), 10, 15); // display game score
-        checkLose(redCar, blueCar); // check lose
-=======
         ctx.fillText("Score:" + " " + Math.floor(count / 100), 10, 15);
+        checkLose(redCar, yellowCar);
         checkLose(redCar, blueCar);
->>>>>>> 170e3d9102cc7a435a333ec26f46cb86eb088583
-        checkLose(redCar, whiteCar);
     }
     requestAnimationFrame(start);
 }
 
 start();
-
-
-
-
